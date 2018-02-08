158f9b2231466567760239b728223846e044cabb
Hannes Karppila <hannes.karppila@gmail.com>
2018-02-08 14:19:48 +0200

    Add printing bot name to the game chat

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # A bot template for [`python-sc2`](https://github.com/Dentosal/python-sc2)
+
+Remember to change the name of your bot from `botinfo.json`.
--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,12 @@
+import json
+from pathlib import Path
+
 import sc2
 
 class MyBot(sc2.BotAI):
+    with open(Path(__file__).parent / "../botinfo.json") as f:
+        NAME = json.load(f)["name"]
+
     async def on_step(self, iteration):
-        pass
+        if iteration == 0:
+            await self.chat_send(f"Name: {self.NAME}")
