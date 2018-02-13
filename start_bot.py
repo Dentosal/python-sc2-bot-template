7befd841f76dfbf5fc9cd8b603bffb1c2989c51b
Hannes Karppila <hannes.karppila@gmail.com>
2018-02-13 08:45:22 +0200

    Add an option for setting log file path

--- start_bot.py
+++ start_bot.py
@@ -1,10 +1,9 @@
 #!python3
-# Usage: python run_external.py [--host]
-
-import argparse
 
 import sys
 import asyncio
+import logging
+import argparse
 
 import sc2
 from sc2 import Race
@@ -12,12 +11,19 @@ from sc2.player import Bot
 
 from bot import MyBot
 
-def main(is_master, map_name, races, portconfig, replay_path):
+def main(is_master, map_name, races, portconfig, replay_path, log_path):
     portconfig = sc2.portconfig.Portconfig.from_json(portconfig)
     i = 0 if is_master else 1
 
     player_config = [Bot(Race[r], None) for r in races]
     player_config[i].ai = MyBot()
+
+    if log_path is not None:
+        logger = logging.getLogger("sc2")
+        logger.setLevel(logging.DEBUG)
+        fh = logging.FileHandler(log_path)
+        fh.setLevel(logging.DEBUG)
+        logger.addHandler(fh)
 
     if is_master:
         g = sc2.main._host_game(
@@ -39,16 +45,21 @@ def main(is_master, map_name, races, portconfig, replay_path):
     print(result)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='Run a game against external server')
     parser.add_argument('--master', action='store_true', help='this is the master (creates the game)')
     parser.add_argument('--replay-path', nargs=1, default='replay.SC2Replay')
-    parser.add_argument('map_name', type=str, help='name of the game map')
-    parser.add_argument('races', type=str, help='player races as comma-separated list')
-    parser.add_argument('portconfig', type=str, help='port configuration as json')
+    parser.add_argument('--log-path', nargs=1, default='-')
+    parser.add_argument('map_name', help='name of the game map')
+    parser.add_argument('races', help='player races as comma-separated list')
+    parser.add_argument('portconfig', help='port configuration as json')
     args = parser.parse_args()
 
     races = args.races.split(",")
 
     assert all(r in ["Random", "Zerg", "Protoss", "Terran"] for r in races)
 
-    main(args.master, args.map_name, races, args.portconfig, args.replay_path[0])
+    main(
+        args.master, args.map_name, races, args.portconfig,
+        args.replay_path[0],
+        args.log_path[0] if args.log_path[0] != "-" else None
+    )
