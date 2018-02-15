12f6953ca4e63ace1bc32cea7744a5b928589c59
Hannes Karppila <hannes.karppila@gmail.com>
2018-02-15 18:55:10 +0200

    Update start_bot script to include game_time_limit parameter

--- start_bot.py
+++ start_bot.py
@@ -11,7 +11,7 @@ from sc2.player import Bot
 
 from bot import MyBot
 
-def main(is_master, map_name, races, portconfig, replay_path, log_path, step_time_limit=None):
+def main(is_master, map_name, races, portconfig, replay_path, log_path, step_time_limit=None, game_time_limit=None):
     portconfig = sc2.portconfig.Portconfig.from_json(portconfig)
     i = 0 if is_master else 1
 
@@ -32,7 +32,8 @@ def main(is_master, map_name, races, portconfig, replay_path, log_path, step_tim
             realtime=False,
             save_replay_as=replay_path,
             portconfig=portconfig,
-            step_time_limit=step_time_limit
+            step_time_limit=step_time_limit,
+            game_time_limit=game_time_limit
         )
     else:
         g = sc2.main._join_game(
@@ -40,7 +41,8 @@ def main(is_master, map_name, races, portconfig, replay_path, log_path, step_tim
             realtime=False,
             save_replay_as=replay_path,
             portconfig=portconfig,
-            step_time_limit=step_time_limit
+            step_time_limit=step_time_limit,
+            game_time_limit=game_time_limit
         )
 
     result = asyncio.get_event_loop().run_until_complete(g)
@@ -51,7 +53,8 @@ if __name__ == '__main__':
     parser.add_argument('--master', action='store_true', help='this is the master (creates the game)')
     parser.add_argument('--replay-path', nargs=1, default='replay.SC2Replay')
     parser.add_argument('--log-path', nargs=1, default='-')
-    parser.add_argument('--step-time-limit', nargs=1, default=None)
+    parser.add_argument('--step-time-limit', nargs=1, default=None, help="Max seconds per step (realtime seconds)")
+    parser.add_argument('--game-time-limit', nargs=1, default=None, help="Max seconds per game (gametime seconds)")
     parser.add_argument('map_name', help='name of the game map')
     parser.add_argument('races', help='player races as comma-separated list')
     parser.add_argument('portconfig', help='port configuration as json')
@@ -65,5 +68,6 @@ if __name__ == '__main__':
         args.master, args.map_name, races, args.portconfig,
         args.replay_path[0],
         args.log_path[0] if args.log_path[0] != "-" else None,
-        float(args.step_time_limit[0]) if args.step_time_limit else None
+        float(args.step_time_limit[0]) if args.step_time_limit else None,
+        float(args.game_time_limit[0]) if args.game_time_limit else None,
     )
