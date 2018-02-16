8d747b80bb21ff1aaac0e3539e65f2547a6e8b57
Hannes Karppila <hannes.karppila@gmail.com>
2018-02-16 08:49:06 +0200

    Update default parameters for run_locally

--- run_locally.py
+++ run_locally.py
@@ -14,7 +14,7 @@ def main():
     run_game(maps.get("Abyssal Reef LE"), [
         Bot(race, MyBot()),
         Computer(Race.Random, Difficulty.Medium)
-    ], realtime=False, step_time_limit=2.0, game_time_limit=20.0)
+    ], realtime=False, step_time_limit=2.0, game_time_limit=(60*20), save_replay_as="test.SC2Replay")
 
 if __name__ == '__main__':
     main()
